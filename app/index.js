12c9babff3315a50f65988d4877009073ba84544
unknown <aidannasca@gmail.com>
2023-05-07 18:26:22 -0400

    hi

--- app/index.js
+++ app/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 const path = require('path');
 var app = express();
-app.use(express.static('./src/'));
+app.use(express.static('src/'));
 app.use(express.static('../contract/build/contracts'));
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, "./src/index.html"));
+  res.sendFile(path.join(__dirname, "src/index.html"));
 });
 var port = process.env.PORT || 3000;
 var host = '0.0.0.0';
