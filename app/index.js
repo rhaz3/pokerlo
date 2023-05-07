f3d1f8fc2b46fdfb541a1994416e831e08a79c11
unknown <aidannasca@gmail.com>
2023-05-07 17:58:12 -0400

    work??

--- app/index.js
+++ app/index.js
@@ -5,6 +5,4 @@ app.use(express.static('../PokerLO-contract/build/contracts'));
 app.get('/', function (req, res) {
   res.render('index.html');
 });
-app.listen(process.env.PORT || 3000, function() {
-  console.log('POKERLO listening on port 3000')
-});
+app.listen(process.env.PORT || 3000);
